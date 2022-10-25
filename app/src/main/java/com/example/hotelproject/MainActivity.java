411f57e3cf680c234496307a227baf570f8df262
mcllontac <116663360+mcllontac@users.noreply.github.com>
2022-10-26 04:21:43 +0800

    Initial commit

--- app/src/main/java/com/example/hotelproject/MainActivity.java
+++ app/src/main/java/com/example/hotelproject/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.hotelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),TermsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),TermsActivity.class);
+                startActivity(i);
+            }
+        });
+
+
+
+
     }
 }
