ada01d4ffca712255d43e4255d1e8cb22b34f27c
mcllontac <116663360+mcllontac@users.noreply.github.com>
2022-10-28 05:13:04 +0800

    Initial commit

--- app/src/main/java/com/example/hotelproject/ReferenceActivity.java
+++ app/src/main/java/com/example/hotelproject/ReferenceActivity.java
@@ -15,8 +15,8 @@ import java.util.Random;
 
 public class ReferenceActivity extends AppCompatActivity {
 
-    Button confirm, gen;
-    String name, contact, persons, checkIn, checkOut, roomType;
+    Button confirm, gen, next;
+    String name1, contact1, persons1, checkIn1, checkOut1, roomType1;
     TextView numRef;
 
     FirebaseDatabase rootNode;
@@ -29,10 +29,11 @@ public class ReferenceActivity extends AppCompatActivity {
         confirm = findViewById(R.id.btnConfirm);
         numRef = findViewById(R.id.refText);
         gen = findViewById(R.id.btnGenerate);
+        next = findViewById(R.id.btnNext);
 
         gen.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+           public void onClick(View view) {
                 Random random = new Random();
                 int val = random.nextInt(999999-100000)+100000;
                 numRef.setText((Integer.toString(val)));
@@ -41,23 +42,39 @@ public class ReferenceActivity extends AppCompatActivity {
 
 
         Intent i = getIntent();
-        name = i.getStringExtra("name");
-        contact = i.getStringExtra("contact");
-        persons = i.getStringExtra("numberofpersons");
-        checkIn = i.getStringExtra("checkin");
-        checkOut = i.getStringExtra("checkout");
-        roomType = i.getStringExtra("roomtype");
+        name1 = i.getStringExtra("name");
+        contact1 = i.getStringExtra("contact");
+        persons1 = i.getStringExtra("numberofpersons");
+        checkIn1 = i.getStringExtra("checkin");
+        checkOut1 = i.getStringExtra("checkout");
+        roomType1 = i.getStringExtra("roomtype");
+
+
+
 
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 rootNode = FirebaseDatabase.getInstance();
-                reference = rootNode.getReference();
+                reference = rootNode.getReference("Reservations");
+
+                String name = name1;
+                String contact = contact1;
+                String persons = persons1;
+                String checkIn = checkIn1;
+                String checkOut = checkOut1;
+                String room = roomType1;
+                String rnum = numRef.getText().toString();
+
+                Helper helper = new Helper(name, contact, persons, checkIn, checkOut, room, rnum);
+
+                reference.child(name).setValue(helper);
+
             }
         });
 
 
-        confirm.setOnClickListener(view -> {
+        next.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), ReferenceNumActivity.class);
             startActivity(intent);
         });
