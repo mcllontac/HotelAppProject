358c540f5ac7638aaee601b6e5055afe80f04e09
Cheesekaykk <116662074+Cheesekaykk@users.noreply.github.com>
2022-10-30 00:46:40 +0800

    main

--- app/src/main/java/com/example/hotelproject/TermsActivity.java
+++ app/src/main/java/com/example/hotelproject/TermsActivity.java
@@ -2,21 +2,31 @@ package com.example.hotelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.LayoutTransition;
 import android.content.Intent;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class TermsActivity extends AppCompatActivity {
 
     Button agree;
     Button back;
 
+    TextView detailsText;
+    LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terms);
 
+
+        // BUTTONS
         agree = findViewById(R.id.btnAgree);
         back = findViewById(R.id.btnBack);
 
@@ -29,5 +39,17 @@ public class TermsActivity extends AppCompatActivity {
             Intent i = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(i);
         });
+
+        // TEXTS
+        detailsText = findViewById(R.id.details);
+        layout = findViewById(R.id.layout);
+        layout.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+    }
+
+    public void expand(View view) {
+        int v = (detailsText.getVisibility() == View.GONE)? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout, new AutoTransition());
+        detailsText.setVisibility(v);
     }
 }
