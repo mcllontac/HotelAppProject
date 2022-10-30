fcd393a7ab3e6a39182e3aa540e382aba33131f9
Cheesekaykk <116662074+Cheesekaykk@users.noreply.github.com>
2022-10-31 01:49:34 +0800

    Activity Rooms and terms are already fixed and the layout is mostly finished might change it again tho

--- app/src/main/java/com/example/hotelproject/RoomsActivity.java
+++ app/src/main/java/com/example/hotelproject/RoomsActivity.java
@@ -3,14 +3,19 @@ package com.example.hotelproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.LayoutTransition;
 import android.content.Intent;
 
 import android.os.Bundle;
 
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class RoomsActivity extends AppCompatActivity {
@@ -21,6 +26,13 @@ public class RoomsActivity extends AppCompatActivity {
 
     Button next;
     Button prev;
+
+    LinearLayout details1Text;
+    LinearLayout details2Text;
+    LinearLayout details3Text;
+    LinearLayout layout1;
+    LinearLayout layout2;
+    LinearLayout layout3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +63,39 @@ public class RoomsActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), TermsActivity.class);
             startActivity(intent);
         });
+
+        details1Text = findViewById(R.id.details1);
+        layout1 = findViewById(R.id.layout1);
+        layout1.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        details2Text = findViewById(R.id.details2);
+        layout2 = findViewById(R.id.layout2);
+        layout2.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
+        details3Text = findViewById(R.id.details3);
+        layout3 = findViewById(R.id.layout3);
+        layout3.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
     }
 
+    public void expand1(View view) {
+        int v = (details1Text.getVisibility() == View.GONE)? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout1, new AutoTransition());
+        details1Text.setVisibility(v);
+    }
+
+    public void expand2(View view) {
+        int x = (details2Text.getVisibility() == View.GONE)? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout2, new AutoTransition());
+        details2Text.setVisibility(x);
+    }
+
+    public void expand3(View view) {
+        int c = (details3Text.getVisibility() == View.GONE)? View.VISIBLE: View.GONE;
+
+        TransitionManager.beginDelayedTransition(layout3, new AutoTransition());
+        details3Text.setVisibility(c);
+    }
 
 }
